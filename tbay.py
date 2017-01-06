c74253ce9f2e24b12a49e4b9101df3918a3d0561
caboe2012 <chad.aboe@gmail.com>
2017-01-06 15:52:13 +0000

    Added Items, Users and Bids models

--- tbay.py
+++ tbay.py
@@ -7,3 +7,36 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, Float
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False)
+    description = Column(String)
+    start_time = Column(DateTime, default = datetime.utcnow)
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key = True)
+    username = Column(String, nullable = False)
+    password = Column(String, nullable = False)
+    
+
+class Bid(Base):
+    __tablename__ = "bids"
+    
+    id = Column(Integer, primary_key = True)
+    price = Column(Float, nullable = False)
+
+
+
+
+
+
+
+Base.metadata.create_all(engine)
